7b3a690e3474ca734fed523117b07ce688dd860a
Mantavya <147939331+mantavya23311@users.noreply.github.com>
2025-04-11 23:45:07 +0530

    Create chart.jsx

---
+++ chart.jsx
@@ -0,0 +1,37 @@
+// components/Chart.tsx
+'use client';
+
+import React from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  CartesianGrid,
+} from 'recharts';
+
+const data = [
+  { name: 'Jan', value: 400 },
+  { name: 'Feb', value: 300 },
+  { name: 'Mar', value: 200 },
+];
+
+const Chart = () => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default Chart;
